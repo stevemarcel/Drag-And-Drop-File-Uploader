392cf25c42367c67e44252b26a74075ddbd879dc
stevemarcel <just.stevemarcel@gmail.com>
2023-04-12 19:44:10 +0100

    Connected frontend to the Node server with upload

--- Backend/server.js
+++ Backend/server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const multer = require("multer");
+const path = require("path");
 const cors = require("cors");
 const colors = require("colors");
 const dotenv = require("dotenv");
@@ -13,20 +14,41 @@ const app = express();
 // Cors Mounting
 app.use(cors());
 
+// serving website on the express server
+app.use("/", express.static(path.join(__dirname, "../Frontend")));
+
 const storage = multer.diskStorage({
-  destination: (req, file, CB) => {
-    CB(null, __dirname + "/uploads");
+  destination: (req, file, cb) => {
+    cb(null, __dirname + "/uploads");
   },
-  filename: (req, file, CB) => {
-    CB(null, file.originalname);
+  filename: (req, file, cb) => {
+    // const fileExt = path.extname(file.originalname);
+    cb(null, Date.now() + "~" + file.originalname);
   },
 });
 
-const uploads = multer({ storage: storage });
+const upload = multer({ storage: storage });
 
-app.post("/uploads", uploads.none(), (req, res, next) => {
-  console.log(req.body);
-  res.json({ status: "files upload successful" });
+// POST Route
+app.post("/uploads", upload.single("uploadedFile"), (req, res, next) => {
+  console.log(req.file);
+  if (req.file === null) {
+    return res.status(400).json({ error: "No File Uploaded!" });
+  }
+  const file = req.file;
+  // Changing generated filename to original filename
+  const fileName = file.filename;
+  let splitFileName = fileName.split("~");
+  const serverFileName = splitFileName[1];
+
+  // Getting file type information
+  const fileType = file.mimetype;
+
+  res.json({
+    serverFileName: serverFileName,
+    filePath: file.path,
+    msg: `${fileType} file uploaded successful`,
+  });
 });
 
 // Start server
--- Frontend/js/script.js
+++ Frontend/js/script.js
@@ -7,7 +7,6 @@ const uploadIcon = document.querySelector("#upload-icon");
 function addBounce() {
   uploadIcon.className = "fa-solid fa-cloud-arrow-up fa-bounce";
 }
-
 function removeBounce() {
   uploadIcon.className = "fa-solid fa-cloud-arrow-up";
 }
@@ -19,28 +18,20 @@ form.addEventListener("click", () => {
 
 fileInput.onchange = ({ target }) => {
   let file = target.files[0];
-  if (file) {
-    // confirming that a file was selected
-    uploadFile(file);
-  }
+  uploadFile(file);
 };
 
 function uploadFile(file) {
-  let payload = new FormData(form);
-  payload.append("uploadedFile", file);
-
   // XMLHttpRequest
   let xhr = new XMLHttpRequest();
-  // xhr.open("POST", "https://httpbin.org/post");
-  xhr.open("POST", "http://localhost:5000/uploads");
-  // xhr.setRequestHeader("Content-Type", "multipart/form-data");
-  xhr.upload.addEventListener("progress", ({ loaded, total }) => {
-    let fileLoaded = Math.floor((loaded / total) * 100); //get percentage of the loaded file
-    let fileTotal = Math.floor(total / 1000); // total file size in KB from bytes
+  // xhr.open("POST", "https://httpbin.org/post"); //Mock API endpoint
+  xhr.open("POST", "/uploads");
 
-    let fileSize;
-    // If the file size is less than 1000 add "KB" else convert to MB and add "MB"
-    fileTotal < 1000 ? (fileSize = fileTotal + " KB") : (fileSize = (fileTotal / 1000).toFixed(2) + " MB");
+  xhr.upload.addEventListener("progress", ({ loaded, total }) => {
+    loadedKB = Math.floor(loaded / 1000); // loaded file size in KB from bytes
+    fileTotalKB = Math.floor(total / 1000); // total file size in KB from bytes
+
+    let fileLoaded = Math.floor((loadedKB / fileTotalKB) * 100); //get percentage of the loaded file
 
     file ? (fileName = file.name) : null; //Setting file name
     // If the file name is longer than or equal to 12 characters, then cut it off at 12 and add file extension at the end
@@ -64,15 +55,33 @@ function uploadFile(file) {
         </li>
     `;
     progressArea.innerHTML = progressAreaHTML;
+  });
 
-    if (loaded == total) {
+  xhr.addEventListener("load", () => {
+    console.log(xhr.status);
+    const res = JSON.parse(xhr.response);
+    console.log(res.msg);
+
+    const serverFileName = res.serverFileName;
+
+    let splitServerFileName = serverFileName.split(".");
+    uploadedFileName = splitServerFileName[0].substring(0, 12) + "... ." + splitServerFileName[1];
+    // let splitServerFileName = serverFileName.split("~");
+    // const uploadedFileName = splitServerFileName[1];
+    console.log(uploadedFileName);
+
+    let fileSize;
+    // If the file size is less than 1000 add "KB" else convert to MB and add "MB"
+    fileTotalKB < 1000 ? (fileSize = fileTotalKB + " KB") : (fileSize = (fileTotalKB / 1000).toFixed(2) + " MB");
+
+    if (loadedKB == fileTotalKB) {
       progressArea.innerHTML = "";
       let uploadedAreaHTML = `
       <li class="row">
             <div class="file-contents">
               <i class="fa-solid fa-file-lines text-light"></i>
               <div class="file-details">
-                <span class="file-name">${fileName} • Uploaded</span>
+                <span class="file-name">${uploadedFileName} • Uploaded</span>
                 <span class="file-size">${fileSize}</span>
               </div>
             </div>
@@ -83,10 +92,12 @@ function uploadFile(file) {
     }
   });
 
-  xhr.addEventListener("load", () => {
-    console.log(xhr.status);
-    console.log(xhr.response);
-  });
+  let payload = new FormData(form);
+  // payload.append("uploadedFile", file);
 
+  // Inspect the payload
+  // for (var pair of payload.entries()) {
+  //   console.log(pair[0] + ", " + pair[1]);
+  // }
   xhr.send(payload);
 }
